b1dc5768aca56691a39b21e70726992bd841746b
Naman Gandhi <121888422+NamanGandhi2005@users.noreply.github.com>
2025-05-26 16:45:04 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -14,7 +14,7 @@ app = FastAPI(title="Dental X-ray Diagnostic Dashboard API")
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"], 
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173","https://dental-frontend-ui.onrender.com"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
